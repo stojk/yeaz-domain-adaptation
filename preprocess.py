9ae544d202036a846185c2dc63c22810cd932bb6
stojk <lazarstojkovic95@gmail.com>
2023-08-19 14:16:22 +0200

    Image intensity rescale in preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -15,11 +15,36 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 
+def rescale_image(
+    image: Image,
+    factor: float
+):
+    """Rescales the image by the given factor. The image is rescaled by multiplying each pixel
+    by the factor. If the resulting value is greater than the maximum possible value of the image,
+    the pixel is set to the maximum possible value.
+
+    Arguments:
+        image: PIL Image
+        factor: factor to rescale the image by
+    """
+    if factor == 1.0:
+        return image
+    I = np.array(image)
+
+    if I.dtype == np.uint8:  # Handle 8-bit images
+        scaled_pixels = np.clip(I * factor, 0, 255)
+        return Image.fromarray(scaled_pixels.astype(np.uint8))
+    elif I.dtype == np.uint16:  # Handle 16-bit images
+        scaled_pixels = np.clip(I * factor, 0, 65535)
+        return Image.fromarray(scaled_pixels.astype(np.uint16))
+    else:
+        raise ValueError("Unsupported image data type")
 
 def crop_into_patches(
     src_path: str,
     dst_path: str,
-    variance_threshold: int,
+    variance_threshold: int = 500000,
+    scale_factor: float = 1.0,
     patch_size: int = 256
 ):
     """Crops the images into patches of size patch_size x patch_size and saves them in the
@@ -28,8 +53,9 @@ def crop_into_patches(
     Arguments:
         src_path: path to the folder containing the images
         dst_path: path to the folder where the patches will be saved
-        variance_threshold: empirical varience threshold
-        patch_size: size of the patches
+        variance_threshold: empirical varience threshold (default: 500000)
+        scale_factor: factor to rescale the images by (default: 1.0)
+        patch_size: size of the patches (default: 256)
     """
 
     # Delete the dst_path if it exists
@@ -40,7 +66,8 @@ def crop_into_patches(
     half_size = patch_size//2
     crop_list = []
     image_list = [
-        Image.open(filename) for filename in glob.glob(op.join(src_path,'*.tif'))
+        rescale_image(Image.open(filename), scale_factor)
+        for filename in glob.glob(op.join(src_path,'*.tif'))
     ]
 
     for image in image_list:
@@ -68,7 +95,9 @@ if __name__ == '__main__':
     parser.add_argument('--dst_path', type=str, required=True,
                         help='Path to the folder where the patches will be saved')
     parser.add_argument('--var_thr', type=int,
-                        required=True, help='Empirical varience threshold')
+                        default=500000, help='Empirical varience threshold')
+    parser.add_argument('--scale_factor', type=float,
+                        default=1.0, help='Factor to rescale the images by')
     parser.add_argument('--patch_size', type=int,
                         default=256, help='Size of the patches')
 
@@ -77,5 +106,6 @@ if __name__ == '__main__':
         src_path=args.src_path,
         dst_path=args.dst_path,
         variance_threshold=args.var_thr,
+        scale_factor=args.scale_factor,
         patch_size=args.patch_size
     )
