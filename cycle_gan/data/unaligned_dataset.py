747b327c057205726f3c4461cd7456a0f832b81b
stojk <lazarstojkovic95@gmail.com>
2023-08-19 08:33:27 +0200

    UnalignedDataset without varience threshold

--- cycle_gan/data/unaligned_dataset.py
+++ cycle_gan/data/unaligned_dataset.py
@@ -51,69 +51,43 @@ class UnalignedDataset(BaseDataset):
             A_paths (str)    -- image paths
             B_paths (str)    -- image paths
         """
-        if self.opt.serial_batches:   # make sure index is within then range
+        if self.opt.serial_batches:   # make sure index is within the range
             index_A = index % self.A_size
         else:   # randomize the index for domain B to avoid fixed pairs.
             index_A = random.randint(0, self.A_size - 1)
         A_path = self.A_paths[index_A]
-        A_img = Image.open(A_path)#.convert('LA')
+        A_img = Image.open(A_path)
 
         if np.array(A_img).dtype.name == 'uint16': #dtype -> dtype.name
             A_img = np.uint8(np.array(A_img) / 256)
             A_img = Image.fromarray(A_img)
 
-        
-        #A_img = A_img.convert('LA')
+        index_B = index
+        B_path = self.B_paths[index_B % self.B_size]  # make sure index is within the range
+        B_img = Image.open(B_path)
+
+        if np.array(B_img).dtype.name == 'uint16': #dtype -> dtype.name
+            B_img = np.uint8(np.array(B_img) / 256)
+            B_img = Image.fromarray(B_img)
 
         # Apply image transformation
         # For FastCUT mode, if in finetuning phase (learning rate is decaying),
         # do not perform resize-crop data augmentation of CycleGAN.
-#        print('current_epoch', self.current_epoch)
         is_finetuning = self.opt.isTrain and self.current_epoch > self.opt.n_epochs
         modified_opt = util.copyconf(self.opt, load_size=self.opt.crop_size if is_finetuning else self.opt.load_size)
         grayscale = (self.opt.input_nc == 1)
         transform = get_transform(modified_opt, grayscale=grayscale, convert=False)
         convert_transform = get_convert_transform(grayscale=grayscale)
-        
+
         if grayscale:
             A_img = A_img.convert('LA')
+            B_img = B_img.convert('LA')
         else:
             A_img = A_img.convert('RGB')
+            B_img = B_img.convert('RGB')
 
-        good_B_found_flag = False
-
-        index_B = index
-        try_counter = 0 # counter of tries to find a good patch on the initially picked image
-
-        while not good_B_found_flag:
-            B_path = self.B_paths[index_B % self.B_size]  # make sure index is within then range
-            B_img = Image.open(B_path)#.convert('LA')
-
-            if np.array(B_img).dtype.name == 'uint16': #dtype -> dtype.name
-                B_img = np.uint8(np.array(B_img) / 256)
-                B_img = Image.fromarray(B_img)
-
-            if grayscale:
-                B_img = B_img.convert('LA')
-            else:
-                B_img = B_img.convert('RGB')
-                
-            B = transform(B_img)
-
-            # check if A varience is high enough (if any cells are present)
-            if np.var(np.array(B)) >= 0:
-                good_B_found_flag = True
-
-            try_counter+=1
-
-            if try_counter > 3:
-                index_B = np.random.randint(self.B_size)
-#        print('1')
-#        print(np.array(A_img).shape, np.array(B_img).shape)
-        B = convert_transform(B)
+        B = convert_transform(transform(B_img))
         A = convert_transform(transform(A_img))
-#        print('2')
-#        print(np.array(A).shape, np.array(B.shape))
         return {'A': A, 'B': B, 'A_paths': A_path, 'B_paths': B_path}
 
     def __len__(self):
